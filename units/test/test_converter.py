2f02e31b8b9ce4b9faa5f9db4dc62c1fdbe1d560
tinnuadan <tinnuadan@gmail.com>
2019-06-30 21:54:26 +0200

    conversion tests

--- units/test/test_converter.py
+++ units/test/test_converter.py
@@ -1,4 +1,5 @@
 import json
+import math
 from ..src.convert_units import converter
 from ..src.convert_units import measurements
 
@@ -37,3 +38,37 @@ def test_findMeasure():
   assert converter.findMeasureForUnit("kn", m) != None
   assert converter.findMeasureForUnit("mph", m) == measure
   
+def test_findConversion():
+  load_helper()
+  
+  assert converter.findConversion("kns") == None
+  assert converter.findConversion("knot").base_measure.unit == "kn"
+  assert converter.findConversion("knots").base_measure.unit == "kn"
+
+def test_conversion_ratio():
+  load_helper()
+
+  c = converter.findConversion("kn")
+  assert c != None
+  conv = c.convert(10)
+  assert conv.measure.unit == "km/h"
+  assert math.isclose(conv.value, 18.52, abs_tol = 0.02)
+  conv = conv.toBest()
+  assert conv.measure.unit == "m/s"
+  assert math.isclose(conv.value, 5.14444, abs_tol = 0.001)
+
+
+def test_conversion_transform():
+  load_helper()
+
+  c = converter.findConversion("C")
+  assert c != None
+  conv = c.convert(10)
+  assert conv.measure.unit == "F"
+  assert math.isclose(conv.value, 50, abs_tol = 0.001)
+
+  c = converter.findConversion("F")
+  assert c != None
+  conv = c.convert(50)
+  assert conv.measure.unit == "C"
+  assert math.isclose(conv.value, 10, abs_tol = 0.001)
