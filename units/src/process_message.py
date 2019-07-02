70396d181bb1e24e2733ed4d6f68248cc9d8823e
tinnuadan <tinnuadan@gmail.com>
2019-07-02 13:37:36 +0200

    message processor

---
+++ units/src/process_message.py
@@ -0,0 +1,115 @@
+import re
+from typing import List, Dict
+from .convert_units import converter
+from .convert_units import measurements
+
+class _MatchToProcess:
+  def __init__(self, match, conversion: converter.Conversion):
+    self.match: re.Match = match
+    self.conversion: converter.Conversion = conversion
+  
+  def convert(self):
+    return self.conversion.convert(self.value())
+    
+  def convertTo(self, to: measurements.Measure):
+    return self.conversion.convertTo(self.value(), to)
+
+  def value(self):
+    value: float = 0
+    tmp: str = self.match.group(1)
+    if ',' in tmp:
+      tmp = tmp.replace(',', '.')
+    value = float(tmp)
+    return value
+
+class _ToConvert:
+  def __init__(self, matchToProces: _MatchToProcess, composedMTP: _MatchToProcess = None, separator: str = None):
+    self.matchToProcess: _MatchToProcess = matchToProces
+    self.composedMTP: _MatchToProcess = composedMTP
+    self.separator: str = separator
+    self.is_composed: bool = composedMTP != None
+
+class _ConversionResult:
+  def __init__(self, orig, conv):
+    self.orig: str = orig
+    self.conv: str = conv
+
+class MessageProcessor:
+  _re: re.Pattern = re.compile(r'(\d+(?:,|\.)?\d*)\s*?([\w\/\'"]+)')  
+
+  
+  def processMessage(self, msg: str):
+    # find all matches
+    matches = self._findMatches(msg)
+    composed = self._findComposed(matches, msg)
+    converted = self._processToConvert(composed)
+
+
+  def _findMatches(self, msg: str):
+    matches = []
+    it = MessageProcessor._re.finditer(msg)
+    for m in it:
+      #search for conversion
+      conversion = converter.findConversion(m.group(2))
+      if conversion:
+        matches.append(_MatchToProcess(m, conversion))
+    return matches
+
+  def _findComposed(self, matches, msg):
+    # find stuff like 5' 11"
+    maxDiff = 3
+    result: List = []
+    skipNext = False
+
+    for i in range(0, len(matches), 1):
+      if skipNext: # Element already handled by composing it with the previous one
+        skipNext = False
+        continue
+
+      cur: _MatchToProcess = matches[i]
+      if i + 1 == len(matches): #we're already at the last element
+        result.append(_ToConvert(cur))
+        break
+      next: _MatchToProcess = matches[i+1]
+      diff = next.match.start() - cur.match.end()
+      
+
+      curConv: converter.Conversion = cur.conversion
+      nextConv: converter.Conversion = next.conversion
+
+      # if the same conversion classes are used and both matches are not far apart
+      if curConv.is_base_metric == nextConv.is_base_metric and curConv.conversion.metric == nextConv.conversion.metric and diff <= maxDiff:
+        separator: str = msg[cur.match.end() : next.match.start()]
+        if len(separator.strip()) == 0: # if there are only whitespaces between the current and the next element
+          # store the composed result
+          result.append(_ToConvert(cur, next, separator))
+          # skip one (i.e. the next) element
+          skipNext = True
+      else:
+        result.append(_ToConvert(cur))
+    return result
+      
+  def _processToConvert(self, matchToProcess):
+    toReplace: List = []
+    for tmp in matchToProcess:
+      toConv: _ToConvert = tmp
+
+      val = toConv.matchToProcess.value()
+
+      if toConv.is_composed:
+        val += toConv.composedMTP.convertTo(toConv.matchToProcess.conversion.base_measure).value
+
+      result: converter.ConversionResult = toConv.matchToProcess.conversion.convert(val)
+      orig: str = str( converter.ConversionResult(val, toConv.matchToProcess.conversion.base_measure) )
+
+      conv: str = str(result)
+      toReplace.append(_ConversionResult(orig, conv))
+    return toReplace
+      
+      
+
+
+
+    
+
+
---
+++ units/test/test_process_message.py
@@ -0,0 +1,84 @@
+import json
+from ..src import process_message
+from ..src.convert_units import converter
+from ..src.convert_units import measurements
+
+def load_helper():
+  c = converter.ConversionData()
+  result = []
+  with open("testdata_ratio.json", "r") as f:
+    data = f.read()
+    measure: dict = json.loads(data)
+    c.metric = measurements.loadMeasurement("ratio", measure["metric"], True)
+    c.imperial = measurements.loadMeasurement("ratio", measure["imperial"], False)
+    result.append(c)
+  
+  c = converter.ConversionData()
+  with open("testdata_transform.json", "r") as f:
+    data = f.read()
+    measure: dict = json.loads(data)
+    c.metric = measurements.loadMeasurement("transform", measure["metric"], True)
+    c.imperial = measurements.loadMeasurement("transform", measure["imperial"], False)
+    result.append(c)
+  
+  converter.conversionsData = result
+
+def test_findMatches():
+  load_helper()
+  msg: str = "Lorem ipsum 10mph dolor sit amet, consectetur 29.1 km/h adipiscing elit 2,16 m/s. Cras accumsan nisi vitae elit suscipit 13.0, luctus pretium 2.3  kn est tincidunt."
+  mp: process_message.MessageProcessor = process_message.MessageProcessor()
+  matches = mp._findMatches(msg)
+
+  assert len(matches) == 4
+  assert matches[0].match.group(1) == "10"
+  assert matches[0].match.group(2) == "mph"
+  assert matches[1].match.group(1) == "29.1"
+  assert matches[1].match.group(2) == "km/h"
+  assert matches[2].match.group(1) == "2,16"
+  assert matches[2].match.group(2) == "m/s"
+  assert matches[3].match.group(1) == "2.3"
+  assert matches[3].match.group(2) == "kn"
+
+def test_findComposed():
+  load_helper()
+  msg: str = "Lorem ipsum 10mph 1 m/s dolor sit amet, consectetur 29.1 km/h 1 m/s adipiscing elit 2,16 m/s. Cras accumsan nisi vitae elit suscipit 13.0, luctus pretium 2.3  kn est tincidunt."
+  mp: process_message.MessageProcessor = process_message.MessageProcessor()
+  
+  matches = mp._findMatches(msg)
+  composed = mp._findComposed(matches, msg)
+
+  assert len(matches) == 6
+  assert len(composed) == 5
+  assert composed[0].is_composed == False
+  assert composed[1].is_composed == False
+  assert composed[2].is_composed == True
+  assert composed[3].is_composed == False
+  assert composed[4].is_composed == False
+  
+  comp: process_message._ToConvert = composed[2]
+  assert comp.separator == r' '
+  assert comp.matchToProcess.match.group(0) == "29.1 km/h"
+  assert comp.composedMTP.match.group(0) == "1 m/s"
+
+def test_processToConvert():
+  load_helper()
+  msg: str = "Lorem ipsum 10mph 1 m/s dolor sit amet, consectetur 29.1 km/h 1 m/s adipiscing elit 2,16 m/s. Cras accumsan nisi vitae elit suscipit 13.0, luctus pretium 2.3  kn est tincidunt."
+  mp: process_message.MessageProcessor = process_message.MessageProcessor()
+  
+  matches = mp._findMatches(msg)
+  composed = mp._findComposed(matches, msg)
+  converted = mp._processToConvert(composed)
+
+  assert len(matches) == 6
+  assert len(composed) == 5
+  assert len(converted) == 5
+  assert converted[0].orig == "10.00 m/h" #ToDo: m/h or mph?
+  assert converted[0].conv == "16.09 km/h" 
+  assert converted[1].orig == "1.00 m/s"
+  assert converted[1].conv == "2.24 m/h" #ToDo: m/h or mph?
+  assert converted[2].orig == "32.70 km/h"
+  assert converted[2].conv == "20.32 m/h" #ToDo: m/h or mph?
+  assert converted[3].orig == "2.16 m/s"
+  assert converted[3].conv == "4.83 m/h" #ToDo: m/h or mph?
+  assert converted[4].orig == "2.30 kn"
+  assert converted[4].conv == "4.26 km/h"
