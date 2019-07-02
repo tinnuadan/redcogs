e4bd67777053d2437b9dae7aea281fef1ecf18d9
tinnuadan <tinnuadan@gmail.com>
2019-07-02 14:46:09 +0200

    Actual conversion

--- units/src/cog.py
+++ units/src/cog.py
@@ -1,10 +1,29 @@
 from redbot.core import commands
+from process_message import MessageProcessor, ConversionResult
+from .convert_units.converter import loadConversionsData
 
 class UnitCog(commands.Cog):
-  """My custom cog"""
+  """Searches for units in a message and tries to convert them from metric to imperial and vice versa"""
+
+  def __init__(self):
+    # load data
+    loadConversionsData()
+    _ = MessageProcessor()
 
   @commands.command()
   async def convert(self, ctx, *, txt):
-    """This does stuff!"""
-    # Your code will go here
-    await ctx.send("Will convert the text %s" % txt)
+    """Do the conversion"""
+    mp: MessageProcessor = MessageProcessor()
+    try:
+      result = mp.processMessage(txt)
+    except Exception as e:
+      print("Exception was thrown while trying to process the message: %s" % e)
+    if len(result) > 0:
+      tmp = []
+      for t in result:
+        cr: ConversionResult = t
+        tmp = "%s = %s" % (cr.orig, cr.conv)
+      answer = "Fyi: %s" % ", ".join(tmp)
+      await ctx.send(answer)
+    else:
+      print("No convertable units found in %s" % txt)
