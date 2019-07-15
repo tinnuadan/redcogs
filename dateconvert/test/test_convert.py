cbcfb77595b6e68dc2504b56eacbff4934ac74ee
tinnuadan <tinnuadan@gmail.com>
2019-07-15 09:18:45 +0200

    advanced tests

--- dateconvert/test/test_convert.py
+++ dateconvert/test/test_convert.py
@@ -43,3 +43,38 @@ def test_convert():
   assert convert_res.time.minute == 0
   assert convert_res.time.second == 0
   assert convert_res.dayShift == 1
+
+def test_differentDaylightSaving():
+  tzs = getTimezones()
+  time = dthandling.TimeObj(12,0,0)
+  date = dthandling.DateObj(2019,10,30) # standard time in europe, still daylight saving in us
+
+  tzinfo = tzs.getTzInfo("CET")
+  convert_from = convert.ConvertFrom(date, time, tzinfo)
+
+  dest_tz = tzs.getTimezone("America/New_York", utils.toUnixTime(convert_from.toDateTime()))
+
+  convert_res = convert.convert(convert_from, dest_tz)
+
+  assert convert_res.date.year == 2019
+  assert convert_res.date.month == 10
+  assert convert_res.date.day == 30
+  assert convert_res.time.hour == 7
+  assert convert_res.time.minute == 0
+  assert convert_res.time.second == 0
+  
+  date = dthandling.DateObj(2019,11,4) # standard time in europe & us
+
+  tzinfo = tzs.getTzInfo("CET")
+  convert_from = convert.ConvertFrom(date, time, tzinfo)
+
+  dest_tz = tzs.getTimezone("America/New_York", utils.toUnixTime(convert_from.toDateTime()))
+
+  convert_res = convert.convert(convert_from, dest_tz)
+
+  assert convert_res.date.year == 2019
+  assert convert_res.date.month == 11
+  assert convert_res.date.day == 4
+  assert convert_res.time.hour == 6
+  assert convert_res.time.minute == 0
+  assert convert_res.time.second == 0
