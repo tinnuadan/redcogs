fdfab870932739bdc3edbe830f5e95cd03f0ef80
Tinnuadan <tinnuadan@gmail.com>
2020-08-04 08:10:44 +0200

    Implementation

--- spam_reminder/src/cog.py
+++ spam_reminder/src/cog.py
@@ -2,28 +2,246 @@ from redbot.core import Config, commands
 import discord
 import typing
 import datetime
+import json
 from . import message_counter
 
 class SpamReminderCog(commands.Cog):
   """Reminds people to switch to another channel if too many messages are posted in a channel which is not meant for discussions"""
 
   def __init__(self):
-    self.counter_mgr = message_counter.MessageCounterMgr()
-    return
+    # self.counter_mgr = message_counter.MessageCounterMgr()
+    self._config = Config.get_conf(self, 15961704848357, force_registration=True)
+    default_user = {"usersr": None}
+    self._config.register_user(**default_user)
+    default_guild = {
+      "msg": "It seems to be an unsually high activity in this channel. You may consider moving the conversation to <alternative>.",
+      "counters": []
+    }
+    self._config.register_guild(**default_guild)
+    self._counters = dict()
+    self._config_loaded = False
+
+  async def auto_load_config(self, guild):
+    if self._config_loaded:
+      return    
+    counters = await self._config.guild(guild).counters()
+    for cnt in counters:
+      data = json.loads(cnt)
+      wc = self.get_channel(guild, data['watched_channel'])
+      ac = self.get_channel(guild, data['channel_out'])
+      counter = message_counter.MessageCounter(wc, ac, int(data['msg_threshold']), int(data['timespan']))
+      self._counters[counter.watched_channel.id] = counter
+    self._config_loaded = True
+    
 
   @commands.Cog.listener()
   async def on_message(self, message: discord.Message):
     if message.author.bot:
       return
+    if message.content[0] == '!':
+      return
     await self.count_messages(message)
 
-  async def count_messages(self, message):
-    res = self.counter_mgr.new_msg(message)
+  async def count_messages(self, message: discord.Message):
+    await self.auto_load_config(message.guild)
+    counter = self.get_counter(message.guild, message.channel)
+    if not counter:
+      return
+    res = counter.new_msg()
     if res == message_counter.MessageCounterResult.OverThreshold:
-      counter = self.counter_mgr.get_counter(message.channel)
-      repl = f"It seems to be an unsually high activity in this channel. You may consider moving the conversation to #{counter.channel.name}"
+      co: discord.TextChannel = counter.channel_out
+      repl = f"It seems to be an unsually high activity in this channel. You may consider moving the conversation to #{co.name}"
       counter.clear()
       await message.channel.send(repl)
+
+  async def get_message(self, guild, channel_name = None) -> str:
+    msg = await self._config.guild(guild).msg()
+    if channel_name:
+      msg = msg.replace("<alternative>", channel_name)
+    return msg
+
+  @commands.group(name="spamreminder", aliases=["sr"])
+  @commands.guild_only()
+  async def spamreminder(self, ctx: commands.Context) -> None:
+    """
+        Monitors channels and friendly reminds people to move the conversation to another channel if there are too many mesages.
+    """
+    pass
+
+  def get_channel(self, guild, channel_name_id):
+    if str(channel_name_id)[0] == "<":
+      channel_name_id = channel_name_id[2:-1]
+    for channel in guild.text_channels:
+      channel: discord.TextChannel = channel
+      if str(channel.id) == str(channel_name_id):
+        return channel
+    return None
+
+  def get_counter(self, guild, channel):
+    if not isinstance(channel, discord.TextChannel):
+      channel = get_channel(guild, channel)
+    if not channel:
+      return None
+    if channel.id in self._counters:
+      return self._counters[channel.id]
+    return None
+
+  @spamreminder.command()
+  async def add(self, ctx, watched_channel, alternative_channel, message_threshold, timespan):
+    """
+        New Monitor for a channel
+    """
+    await self.auto_load_config(ctx.guild)
+
+    try:
+      message_threshold = int(message_threshold)
+    except ValueError:
+      await ctx.send(f"Unable to convert \"{message_threshold}\" to a number")
+      return
+
+    try:
+      timespan = int(timespan)
+    except ValueError:
+      await ctx.send(f"Unable to convert \"{timespan}\" to a number")
+      return
+    
+    wc = self.get_channel(ctx.guild, watched_channel)
+    ac = self.get_channel(ctx.guild, alternative_channel)
+    
+    if not wc:
+      await ctx.send(f"No channel found for \"{watched_channel}\"")
+      return
+    
+    if not ac:
+      await ctx.send(f"No channel found for \"{alternative_channel}\"")
+      return
+
+    if self.get_counter(ctx.guild, wc):
+      await ctx.send(f"Monitor for #{wc.name} already exist.")
+      return
+
+    counters = await self._config.guild(ctx.guild).counters()
+    cntr = message_counter.MessageCounter(wc, ac, message_threshold, timespan)
+    counters.append( cntr.toJson() )
+    self._counters[wc.id] =  cntr
+    await self._config.guild(ctx.guild).counters.set(counters)
+    await ctx.send(f"Monitor for #{wc.name} created.")
+
+  @spamreminder.command()
+  async def list(self, ctx):
+    """
+        Lists all monitors
+    """
+    await self.auto_load_config(ctx.guild)
+    counters = self._counters
+    repl = []
+    for _, cntr in counters.items():
+      repl.append(f"#{cntr.watched_channel.name}: Redirect to #{cntr.channel_out.name} if there are more than {cntr.msg_threshold} with in {cntr.timespan}")
+    if len(repl) == 0:
+      await ctx.send("No monitors set up")
+      return
+    await ctx.send("\n".join(repl))
+
+
+  @spamreminder.command()
+  async def edit(self, ctx, watched_channel, to_edit, value):
+    """
+        Edit monitor of a channel.
+        'to_edit' must be on of the following: 'alternative_channel', 'msg_threshold', 'timespan'
+    """
+    await self.auto_load_config(ctx.guild)
+    to_edits = ['alternative_channel', 'msg_threshold', 'timespan']
+    if to_edit not in to_edits:
+      to_edits = ",".join(to_edits)
+      await ctx.send(f"'to_edit' must be one of the following: {to_edits}")
+      return
+
+    wc = self.get_channel(ctx.guild, watched_channel)
+    if not wc:
+      await ctx.send(f"Channel {watched_channel} not found")
+      return
+
+    cntr = self.get_counter(ctx.guild, wc)
+    if not cntr:
+      await ctx.send(f"There is no monitor set for #{wc.name}")
+      return
+
+    if to_edit == 'alternative_channel':
+      ac = self.get_channel(value)
+      if not ac:
+        await ctx.send(f"No channel found for \"{alternative_channel}\"")
+        return
+      cntr.channel_out = ac
+    
+    if to_edit == 'msg_threshold':
+      try:
+        value = int(value)
+      except ValueError:
+        await ctx.send(f"Unable to convert \"{value}\" to a number")
+        return
+      cntr.msg_threshold = value
+    
+    if to_edit == 'timespan':
+      try:
+        value = int(value)
+      except ValueError:
+        await ctx.send(f"Unable to convert \"{value}\" to a number")
+        return
+      cntr.timespan = value
+
+    counters = await self._config.guild(ctx.guild).counters()
+    for i in range(0, len(counters)):
+      c = json.loads(counters[i])
+      if c['watched_channel'] == cntr.watched_channel.id:
+        counters[i] = cntr.toJson()
+        break
+    self._counters[wc.id] =  cntr
+    await self._config.guild(ctx.guild).counters.set(counters)
+    await ctx.send(f"Monitor for #{wc.name} updated.")
+
+
+  @spamreminder.command()
+  async def remove(self, ctx, watched_channel):
+    """
+        Removes monitoring from a channel
+    """
+    await self.auto_load_config(ctx.guild)
+    wc = self.get_channel(ctx.guild, watched_channel)
+    if not wc:
+      await ctx.send(f"Channel {watched_channel} not found")
+      return
+
+    cntr = self.get_counter(ctx.guild, wc)
+    if not cntr:
+      await ctx.send(f"There is no monitor set for #{wc.name}")
+      return
+
+    counters = await self._config.guild(ctx.guild).counters()
+    for i in range(0, len(counters)):
+      c = json.loads(counters[i])
+      if c['watched_channel'] == cntr.watched_channel.id:
+        del counters[i]
+        break
+    del self._counters[wc.id]
+    await self._config.guild(ctx.guild).counters.set(counters)
+    await self.send(f"Monitor for #{wc.name} removed")
+
+  @spamreminder.command()
+  async def message(self, ctx):
+    """
+        The message which is displayed when there are too many messages. <alternative> will be replaced with the name of the alternative channel.
+    """
+    msg = await self.get_message(ctx.guild)
+    await ctx.send(f"Current message:\n{msg}")
+
+  @spamreminder.command()
+  async def setmessage(self, ctx, message):
+    """
+        The message which is displayed when there are too many messages. <alternative> will be replaced with the name of the alternative channel.
+    """
+    await self._config.guild(ctx.guild).msg.set(message)
+    msg = await self.get_message(ctx.guild)
+    await ctx.send(f"Message changed to:\n{msg}")
 
 
 
--- spam_reminder/src/message_counter.py
+++ spam_reminder/src/message_counter.py
@@ -1,16 +1,19 @@
 from discord import Message, TextChannel
 import time
 import enum
+import json
 
 class MessageCounterResult(enum.Enum):
   WithinThreshold = 0
   OverThreshold = 1
 
 class MessageCounter:
-  def __init__(self, channel_out, msg_threshold, timespan):
+  def __init__(self, channel_watched, channel_out, msg_threshold, timespan):
     self.timestamps = []
     self.msg_threshold = msg_threshold
     self.timespan = timespan
+    self.watched_channel = channel_watched
+    self.channel_out = channel_out
     super().__init__()
 
   def new_msg(self):
@@ -19,6 +22,8 @@ class MessageCounter:
     while True:
       if self.timestamps[0] < now - self.timespan:
         del self.timestamps[0]
+      else:
+        break
     if len(self.timestamps) > self.msg_threshold:
       return MessageCounterResult.OverThreshold
     return MessageCounterResult.WithinThreshold
@@ -26,21 +31,28 @@ class MessageCounter:
   def clear(self):
     self.timestamps.clear()
 
+  def toJson(self):
+    res = dict()
+    res['msg_threshold'] = self.msg_threshold
+    res['timespan'] = self.timespan
+    res['watched_channel'] = self.watched_channel.id
+    res['channel_out'] = self.channel_out.id
+    return json.dumps(res)
 
-class MessageCounterMgr:
-  def __init__(self):
-    self.counters = dict() #TODO: load from config
-    self.counters[646341598623432715] = MessageCounter(646341598623432715, 10, 60)
-    super().__init__()
 
-  def new_msg(self, message: Message):
-    channel: TextChannel = message.channel
-    channel_id = channel.id
-    if channel.id in self.counters:
-      return self.counters[channel.id].new_msg()
-    return MessageCounterResult.WithinThreshold
+# class MessageCounterMgr:
+#   def __init__(self):
+#     self.counters = dict() #TODO: load from config
+#     self.counters[646341598623432715] = MessageCounter(646341598623432715, 2, 60)
+#     super().__init__()
 
-  def get_counter(self, channel):    
-    if channel.id in self.counters:
-      return self.counters[channel.id]
-    return None
+#   def new_msg(self, message: Message):
+#     channel: TextChannel = message.channel
+#     if channel.id in self.counters:
+#       return self.counters[channel.id].new_msg()
+#     return MessageCounterResult.WithinThreshold
+
+#   def get_counter(self, channel):    
+#     if channel.id in self.counters:
+#       return self.counters[channel.id]
+#     return None
